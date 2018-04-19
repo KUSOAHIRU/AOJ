5ed0883a86fc6fdddc716f9535d6afc7cb08b654
KUSOAHIRU <KUSOAHIRU@users.noreply.github.com>
2018-04-19 15:10:51 +0900

    add new file

---
+++ aoj_1-c.cpp
@@ -0,0 +1,11 @@
+//16D8104002I Nozomi Nagaska 長坂望 アカウントIDtanni_kudasai プログラミング言語c++
+
+
+#include <iostream>
+  
+int main(){
+    int a,b;
+    std::cin>>a>>b;
+    std::cout<<a*b<<" "<<(a+b)*2<<std::endl;
+    return 0;
+}
